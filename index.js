d03a665fea394290de4fef0456980144880b8e97
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-02-17 02:45:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  
+  object.assign({}, obj
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
