23f80666ed4d68769e8fc1d7ca29fbaa6861077e
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-02-17 02:40:56 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+//const playlist  = {Queen: "Bohemian Rhapsody"};
+const recipes  = {Pizza: "cheese"};
+
+
+
+function updateObjectWithKeyAndValue(object, key, value){
+  object [key] = value;
+  return object;
+  ///this one is wrong
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object [key] = value;
+  return object;
+}
+//deleteFromObjectByKey(object, key)'
+function destructivelyDeleteFromObjectByKey(object, key){
+  object
+}
+//-----------------
